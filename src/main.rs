f621220df2a7fed7c75eedc60b086d52bf183735
Eddie Cheng <karaburi2023@gmail.com>
2023-11-05 22:25:44 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use coding::string::leetcode557::reverse_words;
+use coding_pk::string::leetcode557::reverse_words;
 
 fn main() {
     let res = reverse_words("Let's take LeetCode contest".to_string());
